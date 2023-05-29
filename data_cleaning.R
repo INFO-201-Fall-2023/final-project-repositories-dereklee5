4f943babcd7041d8b6367592e3a7f781de93ecf7
dereklee5 <122415918+dereklee5@users.noreply.github.com>
2023-05-29 13:32:38 -0700

    Update data_cleaning.R

--- data_cleaning.R
+++ data_cleaning.R
@@ -8,8 +8,32 @@ df <- read.csv("Final_Clean_DF.csv")
 new_df <- df[, c("Year", "Life.Ladder", "Social.Support", "Freedom.To.Make.Life.Choices", "avg_temperature", "avg_oxygen", "avg_nitrate")]
 new_df <- group_by(new_df, Year, avg_temperature, avg_oxygen, avg_nitrate)
 avg_df <- summarize(new_df, life_ladder = mean(Life.Ladder, na.rm=TRUE), social_support = mean(Social.Support, na.rm=TRUE), freedom = mean(Freedom.To.Make.Life.Choices, na.rm=TRUE))
+avg_df$avg_temperature <- (1.8*avg_df$avg_temperature) + 32
+avg_df$life_ladder <- 10*avg_df$life_ladder
+avg_df$social_support <- 100*avg_df$social_support
+avg_df$freedom <- 100*avg_df$freedom
+
+
 data_05 <- filter(avg_df, Year == 2005)
 data_05 <- as.data.frame(t(data_05))
 data_05$Categories <- rownames(data_05)
 data_05 <- subset(data_05, V1 < 1000 )
+
+data_18 <- filter(avg_df, Year == 2018)
+data_18 <- as.data.frame(t(data_18))
+data_18$Categories <- rownames(data_18)
+data_18 <- subset(data_18, V1 < 1000)
+
+#merge_data <- merge(data_05, data_18, by.x = "Categories", by.y = "Categories", all.x = TRUE)
+
+#both_df <- as.data.frame(t(avg_df))
+#both_df$Categories <- rownames(both_df)
+#both_df <- subset(both_df, V1 < 1000)
+column_names <- colnames(avg_df)
+df_new <- data.frame(Variable = rep(column_names, each = nrow(avg_df)),
+                     Value = as.vector(t(avg_df)))
+df_long <- tidyr::gather(avg_df, key = "Variable", value = "Value", -Year)
+b <- ggplot(df_long, aes(x = Year, y = Value, color = Variable)) +
+  geom_line()
+return(b)
 
